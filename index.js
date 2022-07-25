c0631c8ea7d3473b60308e844b67212b0b1389c0
tatarynrm <tatarynrm@gmail.com>
2022-07-25 04:10:38 +0300

    all done

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
-const { Telegraf, Scenes, Markup, session } = require('telegraf')
+const { Telegraf, Scenes, Markup, session } = require('telegraf');
 
+const fs = require('fs');
+const axios = require('axios')
+const dataBase = fs.readFileSync('database.json')
+
+// const RuslanTelegramId = '1060839254';
+// const RuslanTelegramId = '184015320';
 require('dotenv').config();
+
+
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const fs = require('fs')
-const birthdayScene = require('./scenes/birthday.js')
-
+const birthdayScene = require('./scenes/birthday.js');
+const { query, response } = require('express');
 
 const stage = new Scenes.Stage([birthdayScene])
 bot.use(session())
 bot.use(stage.middleware())
 
+bot.hears('text', (ctx) => {
+    console.log(ctx);
+})
+bot.hears('Замовити столик', (ctx) => {
+
+    bot.telegram.sendContact(ctx.chat.id, '+380637444337', 'Yellow Mellow');
+    console.log(ctx.chat.id);
+})
+
+bot.start(async (ctx) => {
+
+    // const chatID = '1060839254';
+    // const chatID = ctx.chat.id;
+    // const chatName = ctx.chat.first_name;
+    // const chatLastName = ctx.chat.last_name;
+    // const chatUserName = ctx.chat.username;
+
+    // const user = {
+    //     chatID,
+    //     chatName,
+    //     chatLastName,
+    //     chatUserName
+    // }
+    // const url = 'https://62a376a85bd3609cee6a9eae.mockapi.io/telegramUsers';
+    // const addUser = async () => {
+    //     const options = {
+    //         url,
+    //         method: 'POST',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json;charset=UTF-8'
+    //         },
+    //         data: {
+    //             chatID,
+    //             chatName,
+    //             chatLastName,
+    //             chatUserName
+    //         }
+    //     };
+
+    //     axios(options)
+    //         .then(response => {
+    //             console.log(response);
+    //         });
+    // }
+    // addUser()
 
 
-
-bot.start((ctx) => {
     bot.telegram.sendMessage(ctx.chat.id,
         `Привіт ${ctx.message.from.username} 
 цей телеграм бот - твій ЄГЕРЬ 
@@ -58,16 +110,26 @@ bot.start((ctx) => {
     })
     ctx.replyWithPhoto({ source: fs.createReadStream('img/main.jpg') })
 })
+// bot.hears('Замовити столик 🪑', (ctx) => {
+//     ctx.replyWithHTML(`Телефон для замовлень:
+
+// 📱 <b><code>+380637444337</code></b>
+// `);
+// })
+
+
+
 bot.hears('Замовити столик 🪑', (ctx) => {
-    ctx.replyWithHTML(`Телефон для замовлень:
-    
-📱 <b><code>+380637444337</code></b>
-`);
+    ctx.reply('Ви можете замовити столик онлайн або зателефонувати нашому менеджеру')
+    bot.telegram.sendContact(ctx.chat.id, '+380637444337', 'Yellow Mellow');
+    console.log(this);
 })
+
 
 bot.hears('Місцезнаходження🎯', (ctx) => {
     bot.telegram.sendLocation(ctx.chat.id, latitude = '49.83870331057144', longitude = '24.027840313759', { proximity_alert_radius: 10 });
 })
+
 
 bot.hears(`Кальянна карта
 😗🚬🌬`, (ctx) => {
@@ -108,7 +170,11 @@ bot.hears('Напої 🧋', (ctx) => {
 })
 // bot.hears('Хочу відсвяткувати ДР у вас!', ctx => ctx.scene.enter('birthdayWizard'))
 
-
+// bot.hears('h', ctx => {
+//     for (let i = 0; i < 100; i++) {
+//         bot.telegram.sendMessage(chat.id, 'Не буду я тебе спамити')
+//     }
+// })
 // кальян
 
 // bot.help((ctx) => ctx.reply('Send me a sticker'))
@@ -122,4 +188,9 @@ bot.launch()
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
+const array = [1, 2, 3, 4, 43430, 434343];
+
+
 
--- scenes/birthday.js
+++ scenes/birthday.js
@@ -2,6 +2,7 @@ const { Markup, Composer, Scenes, Telegraf } = require('telegraf');
 
 const yesUndefined = name => typeof name === 'undefined' ? '' : name;
 
+const data = [];
 const startStep = new Composer();
 startStep.on('text', async (ctx) => {
     try {
@@ -9,7 +10,10 @@ startStep.on('text', async (ctx) => {
         ctx.wizard.state.userName = ctx.message.from.username
         ctx.wizard.state.firstName = ctx.message.from.first_name
         ctx.wizard.state.lastName = ctx.message.from.last_name
-        await ctx.replyWithHTML('<b>Яке число вас цікавить ?</b>')
+        await ctx.replyWithHTML(`
+        <b>Яке число вас цікавить ?</b>
+<i>Наприклад 10 жовтня 2022р</i>
+        `)
 
         return ctx.wizard.next()
     } catch (er) {
@@ -18,16 +22,60 @@ startStep.on('text', async (ctx) => {
 })
 
 
-const dateStep = new Composer()
+const timeStep = new Composer()
 
-dateStep.on('text', async (ctx) => {
+timeStep.on('text', async (ctx) => {
+    try {
+        ctx.wizard.state.data.time = ctx.message.text
+        await ctx.replyWithHTML(`
+        <b>Оберіть час</b>
+<i>Наприклад 15:00</i>
+<b>Зверніть увагу,ми працюємо з 13:00 год</b>
+        `)
+        return ctx.wizard.next()
+
+    } catch (er) {
+        console.log(er);
+    }
+})
+const countStep = new Composer()
+
+countStep.on('text', async (ctx) => {
+    try {
+        ctx.wizard.state.data.count = ctx.message.text
+        await ctx.replyWithHTML(`
+        <b>Кількість осіб</b>
+<i>Наприклад 1-2,3-6,6-12</i>
+        `)
+        return ctx.wizard.next()
+
+    } catch (er) {
+        console.log(er);
+    }
+})
+const contactStep = new Composer()
+
+contactStep.on('text', async (ctx) => {
     try {
         ctx.wizard.state.data.title = ctx.message.text
-        await ctx.replyWithHTML('<b>Оберіть час</b>')
-        return ctx.wizard.next()
+        await ctx.replyWithHTML(`
+        <b>Вкажіть ваші контактні дані(моб.тел)</b>
+<i>Наприклад +380***********</i>
+        `)
+        return ctx.wizard.leave()
+
     } catch (er) {
         console.log(er);
     }
 })
-const birthdayScene = new Scenes.WizardScene('birthdayWizard', startStep, dateStep)
+// const resultStep = new Composer()
+
+// resultStep.leave(async (ctx) => {
+//     ctx.replyWithHTML('hello')
+// })
+
+
+
+const birthdayScene = new Scenes.WizardScene('birthdayWizard', startStep, timeStep, countStep, contactStep,)
+
 module.exports = birthdayScene
